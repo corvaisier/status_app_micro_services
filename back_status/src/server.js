c97ac5a591986bac09c6073645854a1f8f201209
corvaisier <julien.corvaisier.br@gmail.com>
2021-10-21 11:07:29 +0200

    tests for setup

--- back_status/src/server.js
+++ back_status/src/server.js
@@ -1,28 +1,28 @@
 let express = require('express');
 let app = express();
 require('dotenv').config();
+const cors = require('cors')
+app.use(cors());
 
 let mysql = require('mysql');
 
-const database = mysql.createConnection({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: 27017
-    //'mysql://user:pass@host/db?debug=true&charset=BIG5_CHINESE_CI&timezone=-0700'
-});
+// const database = mysql.createConnection({
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+// });
 
-database.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected!");
-});
+// database.connect(function (err) {
+//     if (err) throw err;
+//     console.log("Connected!");
+// });
 
 app.get('/', (req, res) => {
     res.send('Hi There')
 });
 
 app.get('/status', (req, res) => {
-    res.send('ok')
+    res.json({message: 'ok'});
 });
 
-app.listen(process.env.PORT, console.log("app run"));
+app.listen(process.env.PORT, console.log("app run " + process.env.PORT));
