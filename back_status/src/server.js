4c249b7b9cb8ac74c2ecd99d80d440f4a0debc9f
Nicolas LE CHENIC <nicolaslechenic@gmail.com>
2021-10-22 10:16:39 +0200

    fix

--- back_status/src/server.js
+++ back_status/src/server.js
@@ -6,19 +6,17 @@ const cors = require("cors");
 
 app.use(cors());
 
-const mysql = require("mysql");
+const mysql = require("mysql2");
 
 const database = mysql.createConnection({
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  host: 'mysqldb'
 });
 
-database.connect((err) => {
-  if (err) throw err;
-  console.log("Connected!");
-});
+
 
 app.get("/", (req, res) => {
   res.send("Hi There");
